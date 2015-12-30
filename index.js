06d44f05f32df2970cd8b80dee6c2639a2ff9626
Stepan Zastupov <stepan.zastupov@gmail.com>
2015-12-30 04:14:33 +0300

    logging

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 var express = require('express');
 var request = require('request');
+var morgan = require('morgan');
 
 const BOT_URL = "http://localhost:8080"
 
 var app = express();
 
 app.set('port', process.env.PORT || 5000);
-
 app.use(express.static(__dirname + '/dist'));
+app.use(morgan('combined'));
 
 app.get("/tracks", (req, res) => {
   request.get({
