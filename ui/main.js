1bc4d552d6ceed4dbb1a6c376e4003205d774e17
Stepan Zastupov <stepan.zastupov@gmail.com>
2016-01-01 21:23:10 +0300

    Trying fastclick

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,10 @@
 require("./style.less");
 require("whatwg-fetch");
+var FastClick = require('fastclick');
 var React = require("react");
 var ReactDOM = require("react-dom");
 var Player = require("./player").Player;
+
+FastClick.attach(document.body);
 
 ReactDOM.render(<Player/>, document.getElementById('viewport'))
